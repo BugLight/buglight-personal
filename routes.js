295d2c64f81a9a3877a4dc84e4b330527815c60b
Buglight <buglight@kistriver.com>
2017-07-16 15:48:36 +0300

    Reading services list

--- routes.js
+++ routes.js
@@ -1,12 +1,23 @@
 const express = require('express'),
     mongoose = require('mongoose'),
-    models = require('./models'),
+    {Service, Work} = require('./models'),
     router = express.Router();
 
 mongoose.connect(process.env.DB_HOST);
+mongoose.connection.on('error', function (err) {
+    console.error(err);
+});
 
 router.get('/', function (req, res) {
     res.render('index');
+});
+
+router.get('/services/', function (req, res) {
+    Service.find({}).populate('works').exec(function (err, services) {
+        if (err)
+            res.status(500).send('Server error');
+        res.json(services);
+    });
 });
 
 module.exports = router;
