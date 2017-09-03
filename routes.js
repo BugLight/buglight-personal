aaa4294d37625abae7e083d1a7a84dcaa98d0ac1
Buglight <buglight@kistriver.com>
2017-09-03 21:31:36 +0300

    Protected routes

--- routes.js
+++ routes.js
@@ -24,36 +24,44 @@ router.get('/services/', function (req, res) {
 });
 
 router.post('/services/', function (req, res) {
-    Service.create(req.body, function (err) {
-        if (err) {
-            if (err.name === 'ValidationError')
-                res.status(400).json({
-                    message: 'Validation Error',
-                    errors: Object.keys(err.errors)
+    if (!req.user)
+        res.send(401);
+    else {
+        Service.create(req.body, function (err) {
+            if (err) {
+                if (err.name === 'ValidationError')
+                    res.status(400).json({
+                        message: 'Validation Error',
+                        errors: Object.keys(err.errors)
+                    });
+                else
+                    res.send(500);
+            } else {
+                res.json({
+                    message: 'Saved'
                 });
-            else
-                res.send(500);
-        } else {
-            res.json({
-                message: 'Saved'
-            });
-        }
-    });
+            }
+        });
+    }
 });
 
 router.delete('/services/:id', function (req, res) {
-    Service.remove({_id: req.params.id}, function (err) {
-        if (err) {
-            if (err.name === 'CastError')
-                res.send(404);
-            else
-                res.send(500);
-        } else {
-            res.json({
-                message: 'Deleted'
-            });
-        }
-    });
+    if (!req.user)
+        res.send(401);
+    else {
+        Service.remove({_id: req.params.id}, function (err) {
+            if (err) {
+                if (err.name === 'CastError')
+                    res.send(404);
+                else
+                    res.send(500);
+            } else {
+                res.json({
+                    message: 'Deleted'
+                });
+            }
+        });
+    }
 });
 
 router.get('/works/', function (req, res) {
@@ -67,48 +75,56 @@ router.get('/works/', function (req, res) {
 });
 
 router.post('/works/', function (req, res) {
-    Work.create(req.body, function (err, work) {
-        if (err) {
-            if (err.name === 'ValidationError')
-                res.status(400).json({
-                    message: 'Validation Error',
-                    errors: Object.keys(err.errors)
-                });
-            else
-                res.send(500);
-        } else {
-            Service.update({_id: work.service}, {$push: {works: work._id}}, function (err) {
-                if (err)
-                    res.send(500);
-                else {
-                    res.json({
-                        message: 'Saved'
+    if (!req.user)
+        res.send(401);
+    else {
+        Work.create(req.body, function (err, work) {
+            if (err) {
+                if (err.name === 'ValidationError')
+                    res.status(400).json({
+                        message: 'Validation Error',
+                        errors: Object.keys(err.errors)
                     });
-                }
-            });
-        }
-    });
+                else
+                    res.send(500);
+            } else {
+                Service.update({_id: work.service}, {$push: {works: work._id}}, function (err) {
+                    if (err)
+                        res.send(500);
+                    else {
+                        res.json({
+                            message: 'Saved'
+                        });
+                    }
+                });
+            }
+        });
+    }
 });
 
 router.delete('/works/:id', function (req, res) {
-    Work.findByIdAndRemove(req.params.id, function (err, work) {
-        if (err) {
-            if (err.name === 'CastError')
-                res.send(404);
-            else
-                res.send(500);
-        } else {
-            Service.update({_id: work.service}, {$pull: {works: work._id}}, function (err) {
-                if (err)
+    if (!req.user)
+        res.send(401);
+    else {
+        Work.findByIdAndRemove(req.params.id, function (err, work) {
+            if (err) {
+                if (err.name === 'CastError')
+                    res.send(404);
+                else
                     res.send(500);
-                else {
-                    res.json({
-                        message: 'Deleted'
-                    });
-                }
-            });
-        }
-    });
+            } else {
+                Service.update({_id: work.service}, {$pull: {works: work._id}}, function (err) {
+                    if (err)
+                        res.send(500);
+                    else {
+                        res.json({
+                            message: 'Deleted'
+                        });
+                    }
+                });
+            }
+        });
+    }
 });
 
 router.post('/login', passport.authenticate('local'), function (req, res) {
